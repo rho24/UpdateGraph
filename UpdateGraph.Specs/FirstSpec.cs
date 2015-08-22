4a8fe590854ca6169442941816ac3534d1b65b01
Richard Oliver <richard@rholiver.co.uk>
2015-08-22 21:09:40 +0100

    Added specs project

---
+++ UpdateGraph.Specs/FirstSpec.cs
@@ -0,0 +1,13 @@
+﻿using Machine.Specifications;
+using Machine.Specifications.Model;
+
+namespace UpdateGraph.Specs
+{
+    public class FirstSpec
+    {
+        static string _temp;
+        Because of = () => _temp = "Hello";
+
+        It should_work = () => _temp.ShouldEqual("Hello");
+    }
+}
